88b3c9965f0309b2bfff4a9ccd4e0b7bde81c8f5
cheefs <cheefst@outlook.com>
2020-07-09 22:58:17 +0700

    Lesson 5 task 1-2

---
+++ lesson5/Task5-1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/**
+ *  Создать программу, которая будет проверять корректность ввода логина.
+ *  Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы латинского
+        алфавита или цифры, при этом цифра не может быть первой:
+        а) без использования регулярных выражений;
+        б) **с использованием регулярных выражений.
+ */
+
+namespace Task5_1
+{
+    class LoginValidator
+    {
+        const int MIN_LENGTH = 2;
+        const int MAX_LENGTH = 10;
+
+        static bool IsLatinChar( char c )
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');  
+        }
+
+        public static bool Validate( string login )
+        {
+            bool isCorrectLength = (login.Length >= MIN_LENGTH || login.Length <= MAX_LENGTH);
+
+            if ( char.IsNumber( login[0] ) || !isCorrectLength)
+            {
+                return false;
+            }
+
+            foreach ( char el in login )
+            {
+                bool isValidChar = (IsLatinChar(el) || char.IsNumber(el));
+                if ( !isValidChar )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool RegExpValidate( string login )
+        {
+            Regex regex = new Regex(@"^\D[a-zA-z0-9]+$", RegexOptions.IgnoreCase );
+            return regex.IsMatch(login);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter Login: ");
+            string login = Console.ReadLine();
+
+            Console.WriteLine(LoginValidator.Validate(login));
+            Console.WriteLine(LoginValidator.RegExpValidate(login));
+
+            string onlyLatin = "root";
+            Console.WriteLine(LoginValidator.Validate(onlyLatin));
+            Console.WriteLine(LoginValidator.RegExpValidate(onlyLatin));
+
+            string latinWithNumbers = "root123";
+            Console.WriteLine(LoginValidator.Validate(latinWithNumbers));
+            Console.WriteLine(LoginValidator.RegExpValidate(latinWithNumbers));
+
+            string firstNumber = "1root";
+            Console.WriteLine(LoginValidator.Validate(firstNumber));
+            Console.WriteLine(LoginValidator.RegExpValidate(firstNumber));
+
+            string hasKirilic = "roфot";
+            Console.WriteLine(LoginValidator.Validate(hasKirilic));
+            Console.WriteLine(LoginValidator.RegExpValidate(hasKirilic));
+
+            string hasKirilicAndNUmber = "root12Ф";
+            Console.WriteLine(LoginValidator.Validate(hasKirilicAndNUmber));
+            Console.WriteLine(LoginValidator.RegExpValidate(hasKirilicAndNUmber));
+        }
+    }
+}
---
+++ lesson5/Task5-2/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+/**
+ * 2. Разработать методы для решения следующих задач. Дано сообщение:
+    а) Вывести только те слова сообщения, которые содержат не более чем n букв.
+    б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+    в) Найти самое длинное слово сообщения.
+    г) Найти все самые длинные слова сообщения.
+    Постараться разработать класс MyString.
+    Продемонстрируйте работу программы на текстовом файле с вашей программой.
+    д) *Используйте при обработке строк StringBuilder.
+    ***е) Решите задачу, используя только регулярные выражения. 
+ */
+
+namespace Task5_2
+{
+    class MyString
+    {
+        public static string DeleteLongWords( string str, int length, bool regExp = true )
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] arrayOfStrings = str.Split(" ");
+
+            if (regExp)
+            {
+                string noLongWords = Regex.Replace(str, $@"\S{{{length + 1},}}", ""); 
+
+                return Regex.Replace(noLongWords, @"\s{2,}", " ");
+            }
+
+            foreach ( string el in arrayOfStrings )
+            {
+                if ( el.Length > length )
+                {
+                    continue;
+                }
+             
+                sb.Append($"{ el } ");
+            }
+
+            return sb.ToString(); ;
+        }
+
+        public static string DeleteWordsWhoAnds(string str, char symbol, bool regExp = true)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] arrayOfStrings = str.Split(" ");
+
+            if (regExp)
+            {
+                return Regex.Replace(str, @$"\s?\b\S+{ symbol }\b\s?", " ");
+            }
+
+            foreach (string el in arrayOfStrings)
+            {
+                if ( symbol.Equals( el[ el.Length - 1 ]) ) 
+                {
+                    continue;
+                }
+
+                sb.Append($"{ el } ");
+            }
+
+            return sb.ToString(); ;
+        }
+
+        public static string LongestWord( string str )
+        {
+            string[] arrayOfStrings = str.Split(" ");
+  
+            StringBuilder longestWord = new StringBuilder(arrayOfStrings[0]);
+
+            foreach (string el in arrayOfStrings)
+            {
+                if (longestWord.Length > el.Length )
+                {
+                    continue;
+                }
+
+                longestWord = new StringBuilder(el);
+            }
+
+            return longestWord.ToString();
+        }
+
+        public static string GetAllLongestWords(string str)
+        {
+            string longestWord = LongestWord(str);
+
+            MatchCollection m = Regex.Matches(str, $"{longestWord}", RegexOptions.Multiline);
+
+            string matches = "";
+
+            foreach ( Match el in m )
+            {
+                matches += $"{ el } ";
+            }
+
+            return matches;
+        }
+    }
+
+    class Program
+    {
+        const string MESSAGE = "2. Разработать методы для решения следующих задач." +
+            " Дано сообщение:" +
+            " а) Вывести только те слова сообщения которые содержат не более чем n букв." +
+            " б) Удалить из сообщения все слова которые заканчиваются на заданный символ." +
+            " в) Найти самое длинное слово сообщения." +
+            " г) Найти все самые длинные слова сообщения." +
+            " Постараться разработать класс MyString. Продемонстрируйте работу программы на текстовом файле с вашей программой." +
+            " д) * Используйте при обработке строк StringBuilder." +
+            " ***е) Продемонстрируйте Решите задачу, используя только регулярные выражения.";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================================");
+            Console.WriteLine( MyString.DeleteLongWords( MESSAGE, 3, false ) );
+            Console.WriteLine("=================================================");
+            Console.WriteLine(MyString.DeleteLongWords(MESSAGE, 3));
+
+            Console.WriteLine("=================================================");
+            Console.WriteLine(MyString.DeleteWordsWhoAnds(MESSAGE, 'я', false));
+            Console.WriteLine("=================================================");
+            Console.WriteLine(MyString.DeleteWordsWhoAnds(MESSAGE, 'я'));
+            Console.WriteLine("=================================================");
+            Console.WriteLine(MyString.LongestWord(MESSAGE));
+            Console.WriteLine("=================================================");
+            Console.WriteLine(MyString.GetAllLongestWords(MESSAGE));
+        }
+    }
+}
