9c4ca7569e1277766b168db79793e14edbf55984
cheefs <cheefst@outlook.com>
2020-07-11 19:02:27 +0700

    Add changes for lesson code, resolve all tasks

--- lesson6/Task6-3/Program.cs
+++ lesson6/Task6-3/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections.Generic;
 
 /**
- * @todo 
  * Переделать программу Пример использования коллекций для решения следующих задач:
     а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
     б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (*частотный массив);
@@ -24,9 +23,9 @@ namespace Task6_3
         public string department;
         public int group;
         public string city;
-        int age;
+        public int age;
         // Создаем конструктор
-        public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
+        public Student(string firstName, string lastName, string university, string faculty, string department, int age, int course, int group, string city)
         {
             this.lastName = lastName;
             this.firstName = firstName;
@@ -43,27 +42,65 @@ namespace Task6_3
     {
         const string FILE_NAME = "students_1.csv";
 
-        static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+        static int MyDelegat(Student st1, Student st2)
         {
-
-            return String.Compare(st1.firstName, st2.firstName);          // Сравниваем две строки
+            return String.Compare(st1.firstName, st2.firstName);
         }
+
+        static int SortByAge(Student st1, Student st2)
+        {
+            return st1.age.CompareTo(st2.age);
+        }
+
+        static int SortByAgeAndCourse(Student st1, Student st2)
+        {
+            int result = st1.course.CompareTo(st2.course);
+            if (result == 0)
+            {
+                result = st1.age.CompareTo(st2.age);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             int bakalavr = 0;
             int magistr = 0;
+
+            int countStudentsInFifthAndSixCourse = 0;
+
             List<Student> list = new List<Student>();                             // Создаем список студентов
             DateTime dt = DateTime.Now;
             StreamReader sr = new StreamReader(FILE_NAME);
+
+
+            int[] studentsCourseStatistics = new int[ 7 ];
+
             while (!sr.EndOfStream)
             {
                 try
                 {
                     string[] s = sr.ReadLine().Split(';');
                     // Добавляем в список новый экземпляр класса Student
-                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    Student student = new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]);
+                    list.Add(student);
                     // Одновременно подсчитываем количество бакалавров и магистров
-                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++;
+                    if (student.course < 5)
+                    {
+                        bakalavr++;
+                    }
+                    else
+                    {
+                        magistr++;
+                    }
+
+
+                    if ( student.age > 17 && student.age < 21 )
+                    {
+                        studentsCourseStatistics[student.course]++;
+                    }
+                    
+                    countStudentsInFifthAndSixCourse += (student.course >= 5) ? 1 : 0;
                 }
                 catch (Exception e)
                 {
@@ -78,11 +115,26 @@ namespace Task6_3
             Console.WriteLine("Всего студентов:" + list.Count);
             Console.WriteLine("Магистров:{0}", magistr);
             Console.WriteLine("Бакалавров:{0}", bakalavr);
-            foreach (var v in list) Console.WriteLine(v.firstName);
+            // foreach (var v in list) Console.WriteLine(v.firstName);
+
+            Console.WriteLine($"Count students in 5 and 6 course: { countStudentsInFifthAndSixCourse }");
+
+            list.Sort(new Comparison<Student>(SortByAge));
+
+            list.Sort(new Comparison<Student>(SortByAgeAndCourse));
+
+            foreach (Student student in list)
+            {
+                Console.WriteLine($"{student.course } {student.age}");
+            }
+
+            for ( int i = 1; i < studentsCourseStatistics.Length; i++ )
+            {
+                Console.WriteLine($"course{ i }, count: { studentsCourseStatistics[i]} ");
+            }
+
             Console.WriteLine(DateTime.Now - dt);
             Console.ReadKey();
         }
     }
-
-    
 }
